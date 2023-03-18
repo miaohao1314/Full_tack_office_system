2757768178d4a22739a7e77a6bcb6ac8cfb37c3d
miaohao <>
2023-03-18 22:42:04 +0800

    2-4： 自定义异常类

--- emos-wx-api/src/main/java/com/example/emos/wx/exception/EmosException.java
+++ emos-wx-api/src/main/java/com/example/emos/wx/exception/EmosException.java
@@ -2,6 +2,10 @@ package com.example.emos.wx.exception;
 
 import lombok.Data;
 
+/**
+ * 要生成get和set方法，需要继承RuntimeException类
+ */
+
 @Data
 public class EmosException extends RuntimeException {
     private String msg;
